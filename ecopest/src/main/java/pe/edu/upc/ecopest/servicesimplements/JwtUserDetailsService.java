050a936974b2562f3b3e1e1216b7f03c6afe282e
nicopancito <nicosarango10@gmail.com>
2026-09-22 15:56:05 -0500

    adapte las entidades que estaban en ingles al spring security

--- ecopest/src/main/java/pe/edu/upc/ecopest/servicesimplements/JwtUserDetailsService.java
+++ ecopest/src/main/java/pe/edu/upc/ecopest/servicesimplements/JwtUserDetailsService.java
@@ -1,31 +1,30 @@
 package pe.edu.upc.ecopest.servicesimplements;
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import pe.edu.upc.ecopest.entities.Usuario;
-import pe.edu.upc.ecopest.repositories.IUsuarioRepository;
+import pe.edu.upc.ecopest.entities.User;
+import pe.edu.upc.ecopest.repositories.IUserRepository;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
-    private final IUsuarioRepository uR;
+    private final IUserRepository userRepository;
 
-    public JwtUserDetailsService(IUsuarioRepository uR) {
-        this.uR = uR;
+    public JwtUserDetailsService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Usuario usuario = uR.findByEmailUsuario(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
 
-        return User.builder()
-                .username(usuario.getEmailUsuario())
-                .password(usuario.getPasswordUsuario())
-                .authorities(usuario.getRol().getNameRol())
-                .disabled(!usuario.isStatusUsuario())
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRole().getName())
+                .disabled(!user.isActive())
                 .build();
     }
 }
