af056792968797fead2e268553dea79e4655b01a
AdrianoV20 <adriano.vergel.18@gmail.com>
2026-09-23 21:04:01 -0500

    feat: validate login credentials and hide password in requests

--- ecopest/src/main/java/pe/edu/upc/ecopest/dtos/LoginRequestDTO.java
+++ ecopest/src/main/java/pe/edu/upc/ecopest/dtos/LoginRequestDTO.java
@@ -1,8 +1,16 @@
 package pe.edu.upc.ecopest.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class LoginRequestDTO {
+    @NotBlank(message = "The username is required")
     private String username;
 
+    @NotBlank(message = "The password is required")
+    @Size(min = 8, max = 100, message = "The password must be between 8 and 100 characters")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public LoginRequestDTO() {
